33f1aaa3d4f376dc951b09c4f884a61613a2793d
chengkent3 <s2354084@student.hkct.edu.hk>
2023-12-05 14:10:29 +0800

    123

--- app.py
+++ app.py
@@ -1,35 +1,106 @@
-from flask import Flask, request, jsonify
+from flask import Flask,render_template
+from datetime import datetime
+import re
+import json
 
 app = Flask(__name__)
 
-def caesar_encrypt(text, shift):
-    result = ""
-    for char in text:
-        if char.isalpha():
-            ascii_offset = ord('a') if char.islower() else ord('A')
-            result += chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
-        else:
-            result += char
-    return result
+@app.route('/show')
+def home():
+    return render_template('index.html', title='Home', message='Hello, World!')
 
-def caesar_decrypt(text, shift):
-    return caesar_encrypt(text, -shift)
+def encrpyt_code(nums,sourceCode):
+    newStr  = []
+    for str in sourceCode:
+        # print(ord(str))
+        #print(int(ord(str))+int(nums))
+        anum = int(ord(str))
+        rnum = int(nums)
+        if(anum<65 or ( anum>90 and anum<97 ) or anum >122 ):
+            newStr.append(str)
+            continue
+        # print("before anum:{} rnum:{}",anum,rnum)
+        if(anum+rnum>122 and anum <=122):
+            rnum = 122-anum
+            anum=97
+        elif(anum+rnum >90 and anum <=90 ):
+            rnum= 90-anum
+            anum =65
+        # print("anum:{} rnum:{}",anum,rnum)
+        newStr.append(chr((anum+rnum)))
+    print("after sourceCode:{} newStr:{}",sourceCode,newStr)
+    # newStr.reverse()
+    return newStr
 
-@app.route('/encrypt', methods=['POST'])
-def encrypt():
-    data = request.get_json()
-    text = data.get('text')
-    shift = data.get('shift')
-    encrypted_text = caesar_encrypt(text, shift)
-    return jsonify({'encrypted_text': encrypted_text})
 
-@app.route('/decrypt', methods=['POST'])
-def decrypt():
-    data = request.get_json()
-    text = data.get('text')
-    shift = data.get('shift')
-    decrypted_text = caesar_decrypt(text, shift)
-    return jsonify({'decrypted_text': decrypted_text})
+@app.route("/encrypt/<int:nums>/<sourceCode>",methods = ['POST','GET'])
+def encrpyt(nums,sourceCode):
+    sCode =sourceCode
+    encrpytStr= []
 
-if __name__ == '__main__':
-   app.run(debug=True)
+
+    if(int(nums)<=0):
+        return {"code":503,"msg":"nums under 1"}
+    
+    for i in range(int(nums)):
+        encrpytStr.reverse()
+        encrpytStr= encrpyt_code(nums,sourceCode)
+        sourceCode ="".join(encrpytStr)
+    
+    returnStr = {
+        "sourceStr":sCode,
+        "encrpytStr":"".join(encrpytStr),
+        "code":200
+        }
+    return returnStr
+
+
+def decrpyt_code(nums,encrpytCode):
+    
+    newStr  = []
+    reversedStr =[]
+    for str in encrpytCode:
+        reversedStr.append(str)
+    # reversedStr.reverse()
+    # print("encrpytCode:{} reversedStr:{}",encrpytCode,reversedStr)
+    for str in reversedStr:
+        anum = int(ord(str))
+        rnum = int(nums)
+        print("before anum:{} rnum:{}",anum,rnum)
+        if(anum<65 or ( anum>90 and anum<97 ) or anum >122 ):
+            newStr.append(str)
+            continue
+        if(anum-rnum<97 and anum>=97):
+            rnum = anum-97
+            anum=122
+        elif(anum-rnum <65 and anum >=65 ):
+            rnum= anum-65
+            anum =90
+        # print(ord(str))
+        print("after anum:{} rnum:{}",anum,rnum)
+
+        newStr.append(chr((anum-rnum)))
+    return newStr
+
+@app.route("/decrypt/<int:nums>/<encrpytCode>",methods = ['POST','GET'])
+def decrpyt(nums,encrpytCode):
+    sCode =encrpytCode
+    encrpytStr= []
+
+
+    if(int(nums)<=0):
+        return {"code":503,"msg":"nums under 1"}
+    for i in range(int(nums)):
+        encrpytStr.reverse()
+        encrpytStr= decrpyt_code(nums,encrpytCode)
+        encrpytCode ="".join(encrpytStr)
+    returnStr = {
+        "sourceStr":sCode,
+        "encrpytStr":"".join(encrpytStr),
+        "code":200,
+        "msg":"success"
+        }
+    return returnStr
+
+if __name__ == "__main__":  
+  app.run(debug = True)
